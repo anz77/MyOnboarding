a99ef6f1156565dfab5d4b5b48b8a05103b9796e
ANDRII ZUIOK <a.zuiok77@gmail.com>
2021-07-13 19:04:08 +0300

    Initial Commit

---
+++ HolyOnboarding/HolyOnboarding.h
@@ -0,0 +1,18 @@
+//
+//  HolyOnboarding.h
+//  HolyOnboarding
+//
+//  Created by ANDRII ZUIOK on 13.07.2021.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for HolyOnboarding.
+FOUNDATION_EXPORT double HolyOnboardingVersionNumber;
+
+//! Project version string for HolyOnboarding.
+FOUNDATION_EXPORT const unsigned char HolyOnboardingVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <HolyOnboarding/PublicHeader.h>
+
+
